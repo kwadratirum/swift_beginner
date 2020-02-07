c1a2292a1b4003df1eff52fb1a26812239c94075
Ilya <kwadratirum@gmail.com>
2020-02-07 16:25:41 +0300

    Added folder "swift_usov" and completed tasks for 4 chapter

---
+++ swift_usov/glava4.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// Task 7
+
+let myConst = "this is my first const"
+
+// Task 8
+
+var myVariable = 1408
+
+// Task 10
+
+print(myConst)
+
+// Task 11
+
+var age = 19
+print(age)
+age = 20
+print(age)
